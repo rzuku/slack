a610269839bf0f91fff4c38276bb9e1ab3079743
rzuku <48292433+rzuku@users.noreply.github.com>
2020-03-20 19:32:13 +0100

    Add files via upload

---
+++ slack_message.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+# parse arguments
+while getopts "h:c:u:m:" option; do
+  case $option in
+    "h")
+      HOOK=${OPTARG}
+      ;;
+    "c")
+      CHANNEL=${OPTARG}
+      ;;
+    "u")
+      USERNAME=${OPTARG}
+      ;;
+    "m")
+      MESSAGE=${OPTARG}
+      ;;
+    \?|:)
+      echo "Options -u -c -m -h"
+      exit
+      ;;
+    esac
+done
+
+
+
+# prepare message
+MESSAGE_TO_SEND=$(echo -e "${MESSAGE}" | sed 's/\"/\\"/g' | sed "s/'/\'/g" | sed 's/`/\`/g')
+TEXT="\"text\": \"${MESSAGE_TO_SEND}\""
+
+
+
+PAYLOAD="{\"channel\": \"${CHANNEL}\", \"username\":\"${USERNAME}\", ${TEXT}}"
+
+echo $PAYLOAD
+# send message
+curl -s -d "payload=$PAYLOAD" $HOOK
