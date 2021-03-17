45b512c963d68e6021f12f82c905011f1107338d
r-marcin <77101833+r-marcin@users.noreply.github.com>
2021-03-17 13:53:26 +0100

    Update slack_message.sh

--- slack_message.sh
+++ slack_message.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-
 # parse arguments
 while getopts "h:c:u:m:" option; do
   case $option in
@@ -23,16 +21,10 @@ while getopts "h:c:u:m:" option; do
     esac
 done
 
-
-
 # prepare message
 MESSAGE_TO_SEND=$(echo -e "${MESSAGE}" | sed 's/\"/\\"/g' | sed "s/'/\'/g" | sed 's/`/\`/g')
 TEXT="\"text\": \"${MESSAGE_TO_SEND}\""
-
-
-
 PAYLOAD="{\"channel\": \"${CHANNEL}\", \"username\":\"${USERNAME}\", ${TEXT}}"
-
 echo $PAYLOAD
 # send message
 curl -s -d "payload=$PAYLOAD" $HOOK
